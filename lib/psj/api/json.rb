42c700fd40f2bcbed56e0bb4e2edae59ed371d57
Kyle Carter <seijounai@gmail.com>
2013-07-28 10:32:37 -0500

    Reformatted the Json format class.

--- lib/psj/api/json.rb
+++ lib/psj/api/json.rb
@@ -1,6 +1,6 @@
 module PS
   module Api
-    class JSON 
+    class Json 
       include HTTParty
       attr_accessor :apikey, :userkey, :company_name, :env
 
@@ -29,24 +29,49 @@ module PS
       ## }
       #}
       def request(method, params={})
-        results = self.class.post(
-          request_url(method),
-          options_hash(params)
-        )
-        raise results.inspect
+        results = self.class.post(request_url(method), options_hash(params))
         unless results.parsed_response['d']['IsSuccess'] then 
           raise results.parsed_response['d']['ErrorMessage'].inspect
         end
-        results.parsed_response['d']
+        format_response_dates(results.parsed_response['d'])
       end
 
       private
+        #do some conversion for the ASP.net json dates
+        def format_response_dates(response)
+          response["PsObject"].each_with_index do |ps_object, i|
+            ps_object.each do |key, value|
+              if value.instance_of?(String) && value.include?("Date") then
+                response["PsObject"][i][key] = parse_date(value)
+              end
+            end
+          end
+        end
+
+        def format_request_dates(request)
+          request.each do |key, value|
+            if value.instance_of? Time then
+              request[key] = format_date(value)
+            end
+          end
+        end
+
+        #format http://msdn.microsoft.com/en-us/library/bb299886.aspx#intro_to_json_sidebarb 
+        def format_date(date)
+          "/Date(#{(date.to_i*1000)}-0700)/"     
+        end                                            
+
+        def parse_date(str)             
+          Time.at(str[/([0-9]+)-([0-9]+)/,1].to_i/1000)
+        end                                            
+
         def options_hash(post_data)
           post_data[:apikey] = @apikey
           post_data[:userkey] = @userkey
+          post_data = format_request_dates(post_data).to_json
           { 
-          :body => post_data.to_json, 
-          :header => header(post_data.length.to_s)
+          :body => post_data, 
+          :headers => header(post_data.to_json.length.to_s)
           }
         end
 
@@ -55,7 +80,7 @@ module PS
         end
 
         def name 
-          self.class.to_s
+          self.class.name.split('::').last.downcase
         end
         
         def header(content_length)
--- spec/api/json_spec.rb
+++ spec/api/json_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 require File.dirname(__FILE__)+"/../../lib/psj/api/json"
-describe "An instance of", PS::Api::JSON do
+describe "An instance of", PS::Api::Json do
   describe "#request" do
-    let(:config) { formatted_connection_config("JSON") }
+    let(:config) { formatted_connection_config("JSON", :production) }
 
-    subject { PS::Api::JSON.new }
+    subject { PS::Api::Json.new }
 
     before do
-      PS::Base.stub(:host) { "https://sandbox-api.paysimple.com/3.00/paysimpleapi" } 
+      PS::Base.stub(:host) { "https://api.paysimple.com/3.00/paysimpleapi" } 
       subject.instance_variables.each do |v|
         subject.instance_variable_set(v.to_s, config[v.to_s[1..-1].to_sym])
       end
     end
 
-
     it "should make a request" do
-      subject.request("getstates")
+      subject.request("listlineitems")
     end
   end
 end
