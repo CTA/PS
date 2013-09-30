682db3bd458a61dd1ad5ee27d357da54792bbb8b
Kyle Carter <kcarter@ctatechs.com>
2013-09-30 09:45:12 -0500

    fixed some weirdness in recurring payment

--- lib/ps/objects/recurring_payment.rb
+++ lib/ps/objects/recurring_payment.rb
@@ -1,35 +1,48 @@
 module PS
-	class RecurringPayment < Object
-	  attr_accessor :customer_id, :customer_account_id, :recurring_schedule_type, :start_date, :has_end_date, :end_date, :billing_frequency_type, :billing_frequency_param, :payment_amount, :first_payment_done, :first_payment_amount, :first_payment_date, :total_due_amount, :total_number_of_payments, :balance_remaining, :number_of_payments_remaining, :invoice_no, :order_id, :description, :schedule_status, :number_of_payment_made, :total_amount_paid, :date_of_last_payment_made, :pause_until_date
+  class RecurringPayment < Object
+    attr_accessor :customer_id, :customer_account_id, :recurring_schedule_type, :start_date, :has_end_date, :end_date, :billing_frequency_type, :billing_frequency_param, :payment_amount, :first_payment_done, :first_payment_amount, :first_payment_date, :total_due_amount, :total_number_of_payments, :balance_remaining, :number_of_payments_remaining, :invoice_no, :order_id, :description, :schedule_status, :number_of_payment_made, :total_amount_paid, :date_of_last_payment_made, :pause_until_date
 
-		def save
-      request("addrecurringpayment", { :recurring_payment => attributes } )
-		end
-
-		def edit
-			request("modifyrecurringpayment", { :payment_schedule => attributes } )
-		end
-
-		def suspend
-			request("suspendrecurringpaymentschedule", { :schedule_id => self.ps_reference_id } )
-		end
-
-		def resume
-			request("resumerecurringpaymentschedule", { :schedule_id => self.ps_reference_id } )
+    def save
+      begin 
+        save!
+        true
+      rescue Exception
+        false
+      end
     end
 
-	  def destroy
-      request("deleterecurringschedule", { :recurring_payment => attributes } )
-		end	
+    def save!
+      if self.ps_reference_id then
+        update()
+      else
+        request("addrecurringpayment", { :recurringPayment => attributes }, &update_self)
+      end
+    end
 
-		def self.create(params={})
+    def update
+      request("modifyrecurringpayment", { :paymentSchedule => attributes })
+    end
+
+    def suspend
+      request("suspendrecurringpaymentschedule", { :scheduleId => self.ps_reference_id })
+    end
+
+    def resume
+      request("resumerecurringpaymentschedule", { :scheduleId => self.ps_reference_id })
+    end
+
+    def destroy
+      request("deleterecurringschedule", { :recurringPayment => attributes })
+    end	
+
+    def self.create(params={})
       recurring_payment = new(params)
-			recurring_payment.save
-		end
+      recurring_payment.save
+    end
 
-		def self.list(start_date, end_date, customer_id, filter, criteria)
-			request("listrecurringpayments", { :start_date => start_date, :end_date => end_date, :customer_id => customer_id, :ps_recurring_payment_filter => filter, :ps_paged_criteria => criteria } )
-		end
+    def self.list(start_date, end_date, customer_id, filter, criteria)
+      request("listrecurringpayments", { :startDate => start_date, :endDate => end_date, :customerId => customer_id, :filter => filter, :criteria => criteria }, &instantiate_object)
+    end
 
-	end
+  end
 end
